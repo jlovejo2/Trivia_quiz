4a9d2d85c8277304bf67a6dd9b1353529c23ea6c
James Lovejoy <james.lovejoy2@gmail.com>
2019-11-15 17:29:18 -0600

    added wrong answer funtion

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -18,20 +18,26 @@ var quiz_title = document.querySelector("#quiz_title");
 var quiz_instr = document.getElementById("quiz_instr");
 var wrong = document.querySelector(".answer");
 var correct = document.getElementById("correct_answer");
+// var questionEl = 0; 
 // var q1_title = document.questions1.title
+
+var currentQuestionIndex = 0;
+
+// currentQuestionIndex ++;
+// currentQuestionIndex = currentQuestionIndex + 1;
+// // also the same as
+// currentQuestionIndex += 1;
 
 console.log(quiz_content);
 console.log(quiz_title);
 
 
-
 function startQuiz() {
-
-    var x = questions1[0].choices
+    var x = questions1[currentQuestionIndex].choices
 
     document.getElementById("timer").innerHTML = "Timer: " + time;
     interval = setInterval(countDown, 1000)
-    quiz_title.textContent = questions1[0].title;
+    quiz_title.textContent = questions1[currentQuestionIndex].title;
     quiz_q = "";
     quizButton.parentNode.removeChild(quizButton);
     quiz_instr.parentNode.removeChild(quiz_instr);
@@ -47,45 +53,75 @@ function startQuiz() {
         quiz_content.appendChild(br);
         quiz_content.appendChild(hr);
 
-        if (x[i] === questions1[0].answer) {
+        if (x[i] === questions1[currentQuestionIndex].answer) {
             console.log("woohoo!!");
             newEl.setAttribute("id", "correct_answer");
-            newEl.onclick = correct_answer(1);
+            //newEl.onclick = correct_answer;
+            newEl.addEventListener("click", correct_answer);
+        } else {
+            newEl.onclick = wrong_answer;
         }
     }
+
     document.getElementById("correct_answer").setAttribute("style", "color: red;");
 }
 
-function correct_answer(a) {
 
-    var x = questions1[a].choices;
+function correct_answer() {
+    currentQuestionIndex++;
+    var choices = questions1[currentQuestionIndex].choices;
     var answer = document.querySelectorAll(".answer");
+    time = time + 10;
 
-    quiz_title.textContent = questions1[a].title;
-    
-    for (i=0; i < answer.length; i++) {
-    answer[i].innerHTML = x[i];
+    quiz_title.textContent = questions1[currentQuestionIndex].title;
 
-    if (x[i] === questions1[a].answer) {
-        console.log("woohoo!!");
-        answer[i].removeAttribute(onclick);
-        answer[i].setAttribute("id", "correct_answer");
-        answer[i].onclick = correct_answer(2);
-    } else {
-        answer[i].removeAttribute(onclick);
-        answer[i].removeAttribute("id");
-        answer[i].removeAttribute("style");
-        // answer[i].onclick = wrong_answer.
+    for (i = 0; i < answer.length; i++) {
+        answer[i].innerHTML = choices[i];
+
+        if (choices[i] === questions1[currentQuestionIndex].answer) {
+            console.log("woohoo!!");
+            answer[i].removeAttribute(onclick);
+            answer[i].setAttribute("id", "correct_answer");
+            answer[i].onclick = correct_answer;
+            answer[i].setAttribute("style", "color: red;");
+        } else {
+            answer[i].removeAttribute(onclick);
+            answer[i].removeAttribute("id");
+            answer[i].removeAttribute("style");
+            answer[i].onclick = wrong_answer;
+        }
     }
-    }
-    document.getElementById("correct_answer").setAttribute("style", "color: red;");
 }
 
-// function wrong_answer()
+function wrong_answer() {
+    currentQuestionIndex++;
+    var choices= questions1[currentQuestionIndex].choices;
+    var answer = document.querySelectorAll(".answer");
+    time = time - 15;
+
+    quiz_title.textContent = questions1[currentQuestionIndex].title;
+
+    for (i = 0; i < answer.length; i++) {
+        answer[i].innerHTML = choices[i];
+
+        if (choices[i] === questions1[currentQuestionIndex].answer) {
+            console.log("woohoo!!");
+            answer[i].removeAttribute(onclick);
+            answer[i].setAttribute("id", "correct_answer");
+            answer[i].onclick = correct_answer;
+           
+            answer[i].setAttribute("style", "color: red;");
+        } else {
+            answer[i].removeAttribute(onclick);
+            answer[i].removeAttribute("id");
+            answer[i].removeAttribute("style");
+            answer[i].onclick = wrong_answer;
+        }
+    }
+}
 
 function countDown() {
-    document.getElementById("timer").innerHTML = "Timer: " + time;
-
+    
     if (time >= 0) {
         // Get timer to countdown continously
         time--;
@@ -95,7 +131,8 @@ function countDown() {
         clearInterval(time);
         document.getElementById("timer").innerHTML = "EXPIRED";
     }
-
+    
+    document.getElementById("timer").innerHTML = "Timer: " + time;
 }
 
 
